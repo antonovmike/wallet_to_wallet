9a63ea9ef84773853a7737b76872189bf95c2f6f
antonovmike <antonovma@outlook.com>
2023-07-18 12:30:53 +0400

    transfer funds

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import hashlib
 import hmac
 
 # Authentication
+private_key = "qwerty"
 
 # Define the address of the wallet you want to send tokens to
 sender_wallet = "sender_wallet_address"
@@ -15,30 +16,30 @@ amount = 100
 # Define an optional memo describing the transfer
 memo = "Transfer of 100 tokens to wallet abcd1234"
 
-
-def transfer_funds(sender_wallet, receiver_wallet, amount):
-    url = "https://wallet.hiro.so/transfer"
+def transfer_funds(sender_wallet, receiver_wallet, amount, private_key):
+    url = "https://wallet.hiro.so/api/v1/transactions"
     headers = {
-        "Content-Type": "application/json"
+        "Content-Type": "application/json",
+        "X-Hiro-Wallet-Application-Id": "YOUR_APP_ID",
+        "X-Hiro-Wallet-Client-Id": "YOUR_CLIENT_ID"
     }
-    payload = {
-        "sender_wallet": sender_wallet,
-        "receiver_wallet": receiver_wallet,
-        "amount": amount
+    data = {
+        "from_address": sender_wallet,
+        "to_address": receiver_wallet,
+        "amount": amount,
+        "currency": "XRP",
+        "fee": 0.000012,
+        "nonce": 1,
+        "timestamp": 1626580000
     }
-
-    try:
-        response = requests.post(url, json=payload, headers=headers)
-        if response.status_code == 200:
-            return True
-        else:
-            return False
-    except Exception as e:
-        print("An error occurred during the transfer:", e)
-        return False
+    data_json = json.dumps(data)
+    signature = hmac.new(private_key.encode(), msg=data_json.encode(), digestmod=hashlib.sha256).hexdigest()
+    headers["X-Hiro-Wallet-Signature"] = signature
+    response = requests.post(url, headers=headers, data=data_json)
+    return response.json()
 
 
-if transfer_funds(sender_wallet, receiver_wallet, amount):
+if transfer_funds(sender_wallet, receiver_wallet, amount, private_key):
     print("Transfer successful!")
 else:
     print("Transfer failed")
