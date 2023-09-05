a923b6f3720c57b7b2cbcbda50766dfe7124dac8
antonovmike <antonovma@outlook.com>
2023-09-05 11:29:43 +0400

    Invalid length of private key

--- main.py
+++ main.py
@@ -5,14 +5,16 @@ import binascii
 import json
 import hashlib
 
+# Generate
+# Validate and sign
+# Broadcast
 
 private_key = sys.argv[3]
+private_key = bytes(private_key, 'utf-8')
 sender_wallet = sys.argv[1]
 receiver_wallet = sys.argv[2]
 url = "https://wallet.hiro.so/api/v1/transactions"
-
-# Define the amount of tokens you want to send
-amount = 100
+amount = 22
 
 transaction_data = {
     "tx_type": "token_transfer",
@@ -34,12 +36,12 @@ hash_object.update(serialized_tx.encode())
 transaction_hash = hash_object.hexdigest()
 
 # Define an optional memo describing the transfer
-memo = "Transfer of" + str(amount) + "tokens to wallet abcd1234"
+# memo = "Transfer of" + str(amount) + "tokens to wallet" + str(receiver_wallet)
 
 
 def generate_signature():
     sk = ecdsa.SigningKey.from_string(private_key, curve=ecdsa.SECP256k1)
-    signature = sk.sign(transaction_hash)
+    signature = sk.sign(transaction_hash.encode())
     return signature
 
 
